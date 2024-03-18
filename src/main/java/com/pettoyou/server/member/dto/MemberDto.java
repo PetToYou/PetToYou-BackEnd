c0a794eae58976a1450902713ba06452777c4dd9
박성춘 <songchoon10@naver.com>
2024-03-18 23:28:07 +0900

    chore: dto 구조 변경

--- src/main/java/com/pettoyou/server/member/dto/MemberDto.java
+++ src/main/java/com/pettoyou/server/member/dto/MemberDto.java
@@ -1,4 +1,34 @@
 package com.pettoyou.server.member.dto;
 
+import com.pettoyou.server.constant.entity.AuthTokens;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class MemberDto {
+    public static class Request {
+
+        @Data
+        @Builder
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class Login {
+
+        }
+
+
+    }
+
+    public static class Response {
+
+        @Data
+        @Builder
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class SignIn {
+            private AuthTokens authTokens;
+            private String nickname;
+        }
+    }
 }
