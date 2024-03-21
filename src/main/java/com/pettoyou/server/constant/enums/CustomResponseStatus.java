b744bbd41019f48453327b2bcd3838811719856e
박성춘 <songchoon10@naver.com>
2024-03-21 12:39:55 +0900

    feat: 공통 예외 처리 로직 구현

--- src/main/java/com/pettoyou/server/constant/enums/CustomResponseStatus.java
+++ src/main/java/com/pettoyou/server/constant/enums/CustomResponseStatus.java
@@ -29,13 +29,17 @@ public enum CustomResponseStatus {
     NULL_JWT(HttpStatusCode.valueOf(HttpStatus.NO_CONTENT.value()), "4000", "토큰이 공백입니다."),
     MEMBER_NOT_FOUND(HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value()), "4001", "해당 유저를 찾을 수 없습니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value()), "4002", "리프레시 토큰을 찾을 수 없습니다."),
-    ROLE_NOT_FOUND(HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value()), "4003", "해당 권하을 찾을 수 없습니다."),
-
+    ROLE_NOT_FOUND(HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value()), "4003", "해당 권한을 찾을 수 없습니다."),
 
     /***
      * 5000: NOT_MATCH
      */
-    REFRESH_TOKEN_NOT_MATCH(HttpStatusCode.valueOf(HttpStatus.CONFLICT.value()), "5000", "잘못된 리프레시 토큰입니다.");
+    REFRESH_TOKEN_NOT_MATCH(HttpStatusCode.valueOf(HttpStatus.CONFLICT.value()), "5000", "잘못된 리프레시 토큰입니다."),
+
+    /***
+     * 6000: Internal_Server_Error
+     */
+    INTERNAL_SERVER_ERROR(HttpStatusCode.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), "6000", "내부 서버 오류입니다.");
 
 
     private final HttpStatusCode httpStatusCode;
