2d3bb2bbf8f98933b5b809f28f73a4b49fce4fd4
박성춘 <songchoon10@naver.com>
2024-07-11 12:41:04 +0900

    fix: 필요없는 필터링조건 삭제

--- src/main/java/com/pettoyou/server/hospital/dto/request/HospitalQueryCond.java
+++ src/main/java/com/pettoyou/server/hospital/dto/request/HospitalQueryCond.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 public record HospitalQueryCond(
         @Nullable String businessHourCond,
-        @Nullable Integer distanceCond, // 프론트에서 보내줄때 화면에서 보이는것과 달리 meter 단위로 보내줘야함.
         @Nullable String specialitiesCond,
         @Nullable String filter,
         @Nullable Integer radius,
