41376d6864444f8c4eaa1a6db1010d2c2264db65
박성춘 <songchoon10@naver.com>
2024-07-30 00:30:54 +0900

    #48 refactor: 메서드분리

--- src/main/java/com/pettoyou/server/scrap/service/ScrapServiceImpl.java
+++ src/main/java/com/pettoyou/server/scrap/service/ScrapServiceImpl.java
@@ -10,13 +10,11 @@ import com.pettoyou.server.scrap.dto.response.ScrapQueryRespDto;
 import com.pettoyou.server.scrap.dto.response.ScrapRegistRespDto;
 import com.pettoyou.server.scrap.entity.Scrap;
 import com.pettoyou.server.scrap.repository.ScrapRepository;
-import com.pettoyou.server.store.entity.Store;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -27,13 +25,9 @@ public class ScrapServiceImpl implements ScrapService {
     private final HospitalRepository hospitalRepository;
 
     @Override
-    public ScrapRegistRespDto scrapRegist(Long storeId, Long memberId) {
-        Member findMember = memberRepository.findById(memberId).orElseThrow(() ->
-                new CustomException(CustomResponseStatus.MEMBER_NOT_FOUND)
-        );
-        Hospital findHospital = hospitalRepository.findById(storeId).orElseThrow(
-                () -> new CustomException(CustomResponseStatus.HOSPITAL_NOT_FOUND)
-        );
+    public ScrapRegistRespDto scrapRegist(Long storeId, Long authMemberId) {
+        Member findMember = findMemberById(authMemberId);
+        Hospital findHospital = findHospitalById(storeId);
 
         Scrap saveScrap = scrapRepository.save(Scrap.of(findMember, findHospital));
 
@@ -42,13 +36,9 @@ public class ScrapServiceImpl implements ScrapService {
 
     @Override
     public void scrapCancel(Long scrapId, Long authMemberId) {
-        Scrap findScrap = scrapRepository.findById(scrapId).orElseThrow(() ->
-                new CustomException(CustomResponseStatus.SCRAP_NOT_FOUND)
-        );
+        Scrap findScrap = findScrapById(scrapId);
+        Member findMember = findMemberById(authMemberId);
 
-        Member findMember = memberRepository.findById(authMemberId).orElseThrow(() ->
-                new CustomException(CustomResponseStatus.MEMBER_NOT_FOUND)
-        );
         findMember.validateMemberAuthorization(authMemberId);
 
         scrapRepository.delete(findScrap);
@@ -57,5 +47,23 @@ public class ScrapServiceImpl implements ScrapService {
     @Override
     public List<ScrapQueryRespDto> fetchScrapStore(Long memberId) {
         return scrapRepository.findScrapListByMemberId(memberId);
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() ->
+                new CustomException(CustomResponseStatus.MEMBER_NOT_FOUND)
+        );
+    }
+
+    private Hospital findHospitalById(Long hospitalId) {
+        return hospitalRepository.findById(hospitalId).orElseThrow(
+                () -> new CustomException(CustomResponseStatus.HOSPITAL_NOT_FOUND)
+        );
+    }
+
+    private Scrap findScrapById(Long scrapId) {
+        return scrapRepository.findById(scrapId).orElseThrow(() ->
+                new CustomException(CustomResponseStatus.SCRAP_NOT_FOUND)
+        );
     }
 }
